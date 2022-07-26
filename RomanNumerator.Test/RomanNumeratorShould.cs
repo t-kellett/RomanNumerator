57be1dc029e304d5e402ad85a1e73851845ec943
Tom Kellett <tomkellett@Toms-MacBook-Pro.local>
2022-07-26 16:59:45 +0100

    Refining test structure

---
+++ RomanNumerator.Test/RomanNumeratorShould.cs
@@ -0,0 +1,40 @@
+﻿using RomanNumerator;
+
+namespace RomanNumerator.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCase(1, "I")]
+    [TestCase(2, "II")]
+    [TestCase(3, "III")]
+    [TestCase(4, "IV")]
+    [TestCase(5, "V")]
+    [TestCase(6, "VI")]
+    [TestCase(7, "VII")]
+    [TestCase(8, "VIII")]
+    [TestCase(9, "IX")]
+    [TestCase(10, "X")]
+    [TestCase(11, "XI")]
+    [TestCase(24, "XXIV")]
+    [TestCase(38, "XXXVIII")]
+    [TestCase(40, "XL")]
+    [TestCase(50, "L")]
+    [TestCase(90, "XC")]
+    [TestCase(100, "C")]
+    [TestCase(400, "CD")]
+    [TestCase(500, "D")]
+    [TestCase(900, "CM")]
+    [TestCase(1000, "M")]
+    [TestCase(2499, "MMCDXCIX")]
+    [TestCase(3949, "MMMCMXLIX")]
+
+    public void ShouldConvertIntegerToNumerals(int input, string output)
+    {
+        Assert.That(Numerator.Convert(input), Is.EqualTo(output));
+    }
+}
