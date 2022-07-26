ceec533305b0af2654ac8b04949114a446a1ec3b
Tom Kellett <tomkellett@Toms-MacBook-Pro.local>
2022-07-26 13:48:53 +0100

    Adding 1 to 10 with passing unit tests

---
+++ Numerator.Test/ShouldConvertDigits.cs
@@ -0,0 +1,26 @@
+﻿using RomanNumerator;
+
+namespace RomanNumerator.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCase("I", 1)]
+    [TestCase("II", 2)]
+    [TestCase("III", 3)]
+    [TestCase("IV", 4)]
+    [TestCase("V", 5)]
+    [TestCase("VI", 6)]
+    [TestCase("VII", 7)]
+    [TestCase("VIII", 8)]
+    [TestCase("IX", 9)]
+    [TestCase("X", 10)]
+    public void ShouldConvertIntegerToNumerals(string output, int input)
+    {
+        Assert.That(Numerator.Convert(input), Is.EqualTo(output));
+    }
+}
--- RomanNumerator/Numerator.cs
+++ RomanNumerator/Numerator.cs
@@ -1,6 +1,35 @@
 ﻿namespace RomanNumerator;
+
 public class Numerator
 {
+    static Dictionary<int, string> NumberToRoman = new Dictionary<int, string>
+    {
+        { 4, "IV" },
+        { 9, "IX" },
+        { 10, "X" }
+    };
+
+    public static string Convert(int i)
+    {
+        string output = "";
+
+        if (NumberToRoman.Keys.Contains(i))
+        {
+            return output += NumberToRoman[i];
+        }
+
+        for (var n = 0; n < i; n++)
+        {
+            if (i >= 5)
+            {
+                output += "V";
+                i -= 4;
+                continue;
+            }
+            output += "I";
+        }
+        return output;
+    }
 
 }
 
